07347943f6bef8a687304c7ae2cc2c898796df3e
Nicole Candiotti <nccandiotti@gmail.com>
2022-01-11 20:18:14 -0500

    commit tree b6215dfd159bb875e8e3692ca68aafd7faa2d4fe

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+"strict";
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+  var customerName = "bob";
+  return customerName.toUpperCase();
+}
+upperCaseCustomerName(customerName);
+function setBestCustomer() {
+  var bestCustomer = "notBob";
+  console.log(bestCustomer);
+}
+function setBestCustomer() {
+  var bestCustomer = "not bob";
+  return bestCustomer;
+}
+// function setBestCustomer() {
+//   var bestCustomer = "not bob";
+// }
+// function overwriteBestCustomer() {
+//   var bestCustomer = "Sam";
+// }
